dd0154d333490edddcdefb765d3e4b15f6b4552e
keyneth7 <keylara7@gmail.com>
2024-01-23 16:10:25 -0500

    Agregado contador de intentos

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let numeroSecreto = 6;
 let numeroUsuario = 0;
+let intentos = 1; 
+let palabraVeces = "vez"
 
 while (numeroSecreto != numeroUsuario) {
     numeroUsuario = prompt("Ingresa un número entre 1 y 10, por favor:");
@@ -7,12 +9,14 @@ while (numeroSecreto != numeroUsuario) {
     console.log(numeroUsuario);
 
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el número es ${numeroUsuario}`);
+        alert(`Acertaste, el número es ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
         } else {
             alert('El número secreto es mayor');
         }
+        intentos = intentos + 1;
+        palabraVeces = 'veces'
     }
 }
