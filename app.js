d1f09154474e1ec25996e27afcbb71fb917f2f59
keyneth7 <keylara7@gmail.com>
2024-01-23 17:05:39 -0500

    Funcionalidad pseudoaleatoria

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-let numeroSecreto = 6;
+let numeroSecreto = Math.floor(Math.random()*10) + 1;
 let numeroUsuario = 0;
 let intentos = 1; 
-let palabraVeces = "vez"
+let maxIntentos = 3;
+
+console.log(numeroSecreto);
 
 while (numeroSecreto != numeroUsuario) {
-    numeroUsuario = prompt("Ingresa un número entre 1 y 10, por favor:");
+    numeroUsuario = parseInt(prompt("Ingresa un número entre 1 y 10, por favor:"));
 
     console.log(numeroUsuario);
 
     if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el número es ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`);
+        alert(`Acertaste, el número es ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
     } else {
         if (numeroUsuario > numeroSecreto) {
             alert('El número secreto es menor');
         } else {
             alert('El número secreto es mayor');
         }
-        intentos = intentos + 1;
-        palabraVeces = 'veces'
+        intentos++;
+        if (intentos > maxIntentos){
+            alert(`Llegaste al número max. de ${maxIntentos} intentos`);
+            break; 
+        }
     }
 }
