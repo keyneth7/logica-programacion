b61613d812b84c781bbbe3467c2ba1ceb43ee65c
keyneth7 <keylara7@gmail.com>
2024-01-23 15:54:37 -0500

    Agregados bucle y pistas

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Ingresa un número entre 1 y 10, por favor:");
+let numeroUsuario = 0;
 
-console.log(numeroUsuario);
+while (numeroSecreto != numeroUsuario) {
+    numeroUsuario = prompt("Ingresa un número entre 1 y 10, por favor:");
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el número');
-} else {
-    alert('Lo siento, no acertaste el número');
+    console.log(numeroUsuario);
+
+    if (numeroUsuario == numeroSecreto) {
+        alert(`Acertaste, el número es ${numeroUsuario}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+            alert('El número secreto es menor');
+        } else {
+            alert('El número secreto es mayor');
+        }
+    }
 }
