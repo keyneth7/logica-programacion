205d087bdd740b69963e2881b965a20e6db94c21
keyneth7 <keylara7@gmail.com>
2024-02-07 17:59:28 -0500

    Mejora de funcionalidades

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-let numeroUsuario = 0;
-let intentos = 1; 
-let maxIntentos = 3;
+let numeroSecreto = 0
+let numeroIntentos = 0
+let intentos = 0
 
-let numeroMax = parseInt(prompt('Ingresa hasta que número quieres jugar'));
-let numeroSecreto = Math.floor(Math.random()*numeroMax) + 1;
-
-while (numeroSecreto != numeroUsuario) {
-    numeroUsuario = parseInt(prompt(`Ingresa un número entre 1 y ${numeroMax}, por favor:`));
-
-    if (numeroUsuario == numeroSecreto) {
-        alert(`Acertaste, el número es ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez.' : 'veces.'}`);
-    } else {
-        if (numeroUsuario > numeroSecreto) {
-            alert('El número secreto es menor');
-        } else {
-            alert('El número secreto es mayor');
-        }
-        intentos++;
-        if (intentos > maxIntentos){
-            alert(`Llegaste al número max. de ${maxIntentos} intentos. El número era ${numeroSecreto}.`);
-            break; 
-        }
-    }
+function asignarTexto(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
 }
+
+function verificarIntento() {
+    let numeroUsuario = parseInt(document.getElementById('numeroUsuario').value);
+    if (numeroSecreto == numeroUsuario){
+        asignarTexto('p',`Acertaste el número en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (numeroSecreto>numeroUsuario){
+            asignarTexto('p','El número secreto es mayor');
+        } else {
+            asignarTexto('p','El número secreto es menor');
+        }
+        limpiarCaja();
+        intentos++;
+        console.log(intentos);
+    }
+    return;
+}
+
+function generarNumero() {
+    return Math.floor(Math.random()*10)+1;
+}
+
+function limpiarCaja(){
+    valorCaja = document.querySelector('#numeroUsuario').value = '';
+}
+
+function condicionesIniciales(){
+    asignarTexto('h1', 'Juego del número secreto');
+    asignarTexto('p', 'Indica un número del 1 al 10');
+    numeroSecreto = generarNumero();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled', 'true');
+}
+
+condicionesIniciales();
